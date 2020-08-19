4a150c75d768b7546b0bbaee19f0bf50f568a58b
ajanee5r <69921323+ajanee5r@users.noreply.github.com>
2020-08-19 15:02:38 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*********************************************************
+* Hello.java
+****************************************
+* Ajanee Robinson
+* 08/19/2020
+* CMSC 255 008
+**********************************************************/
+
+public class Hello {
+   public static void main(String[] args) {
+
+   	System.out.println("Ajanee Robinson");
+   	System.out.println("Hallo.java");
+   	System.out.println("project Description");
+   	System.out.println("08/19/2020");
+   	System.out.println("CSMC 255 008");
+   	System.out.println();
+   	}
+ }
